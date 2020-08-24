9be14bd494ba5766669e1b113b2ea75468b82440
goreev <goreev.ar@gmail.com>
2020-08-24 14:31:54 +0300

    auth reducer

--- src/index.js
+++ src/index.js
@@ -7,11 +7,13 @@ import { createStore, applyMiddleware, compose, combineReducers } from "redux";
 import { Provider } from "react-redux";
 import burgerBuilder from "./store/reducers/burgerBuilder";
 import ordersReducer from "./store/reducers/orders";
+import authReducer from "./store/reducers/auth";
 import thunk from "redux-thunk";
 
 const rootReducer = combineReducers({
   burgerBuilder: burgerBuilder,
   orders: ordersReducer,
+  auth: authReducer,
 });
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
